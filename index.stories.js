8ba952c5011128dd879bfe3789d30e074394d2e2
ggolfthanakorn <40067627+ggolfthanakorn@users.noreply.github.com>
2018-06-11 16:23:39 +0700

    Update index.stories.js

--- index.stories.js
+++ index.stories.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { storiesOf } from '@storybook/react';
 
 import fontawesome from '@fortawesome/fontawesome'
@@ -5,24 +7,19 @@ import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import brands from '@fortawesome/fontawesome-free-brands'
 import faCoffee from '@fortawesome/fontawesome-free-solid/faCoffee'
 
-import PltButtonNormalIcon from './PltButtonNormalIcon';
-import PltButtonHoverIcon from './PltButtonHoverIcon';
-import PltButtonActiveIcon from './PltButtonActiveIcon';
-import PltButtonFocusIcon from './PltButtonFocusIcon';
-import PltButtonDisIcon from './PltButtonDisIcon';
+import Pltbtn from './Pltbtn';
+import PltbtnDis from './PltbtnDis';
+
+import Pltlink from './Pltlink';
+
+import Pltloading from './Pltloading';
+
+import PltStepFlow from './PltStepFlow';
 
 fontawesome.library.add(brands, faCoffee)
 
-storiesOf('PltButtonIcon', module)
-    .add('btnPltNormIcon', () => <PltButtonNormalIcon text="hello"  btnPlt="btnPlt" icon={<FontAwesomeIcon icon="coffee" />} />  )
-
-    .add('btnPltHoverIcon', () => <PltButtonHoverIcon text="hello"  btnPltHover="btnPltHover" icon={<FontAwesomeIcon icon="coffee" />} /> )
- 
-    .add('btnPltActiveIcon', () => <PltButtonActiveIcon text="hello"  btnPltActive="btnPltActive" icon={<FontAwesomeIcon icon="coffee" />} /> )
-
-    .add('btnPltFocusIcon', () => <PltButtonFocusIcon text="hello"  btnPltFocus="btnPltFocus"  icon={<FontAwesomeIcon icon="coffee" />} /> )
-
-   .add('btnPltDisIcon', () => <PltButtonDisIcon text="hello"  btnPltDis="btnPltDis"  icon={<FontAwesomeIcon icon="coffee" />}/> )
-
-
-;
+storiesOf('Pltbtn', module)
+    .add('btnPlt', () => <Pltbtn text="hello"  btnPlt="btnPlt" icon={<FontAwesomeIcon icon="coffee" />} />  )
+         
+    .add('btnPltDis', () => <PltbtnDis  text="hello"  btnPltDis="btnPltDis" icon={<FontAwesomeIcon icon="coffee" />} iconRight/>  )
+         ;
